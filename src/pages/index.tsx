6ea3fcd69382816071011ff31fc6fc05974d1b44
Miguel Rocha <miguelrocha.dev@gmail.com>
2022-11-05 01:44:33 +0000

    add round count and winner message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,36 @@ import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { Scoreboard } from "../components/Scoreboard";
 import {
+  Evaluation as IEvaluation,
+  GetCurrentWinnerMessageDocument,
+  GetCurrentWinnerMessageQuery as IGetCurrentWinnerMessageQuery,
+  GetEvaluationsDocument,
+  GetEvaluationsQuery as IGetEvaluationsQuery,
   GetPlayersDocument,
   GetPlayersQuery as IGetPlayersQuery,
   Player as IPlayer,
+  WinnerMessage as IWinnerMessage,
 } from "../graphql/schema";
 import { client } from "../lib/apollo";
 import { styled } from "../stitches/config";
 
 const Main = styled("main", {
   display: "flex",
+  flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
 
+  gap: "2rem",
+
   padding: "2rem",
+});
+
+const Round = styled("h1", {
+  textAlign: "center",
+});
+
+const Message = styled("h2", {
+  textAlign: "center",
 });
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -24,13 +41,31 @@ export const getStaticProps: GetStaticProps = async () => {
     query: GetPlayersDocument,
   });
 
+  const {
+    data: { evaluations },
+  } = await client.query<IGetEvaluationsQuery>({
+    query: GetEvaluationsDocument,
+  });
+
+  const {
+    data: { winnerMessages: currentWinnerMessage },
+  } = await client.query<IGetCurrentWinnerMessageQuery>({
+    query: GetCurrentWinnerMessageDocument,
+  });
+
   return {
-    props: { players },
-    revalidate: 60 * 60 * 2, // 2 hours
+    props: { players, evaluations, currentWinnerMessage },
+    revalidate: 60 * 10, // 10 minutes
   };
 };
 
-const HomePage: NextPage<{ players: IPlayer[] }> = ({ players }) => {
+const HomePage: NextPage<{
+  players: IPlayer[];
+  evaluations: IEvaluation[];
+  currentWinnerMessage: IWinnerMessage[];
+}> = ({ players, evaluations, currentWinnerMessage }) => {
+  const currentMessage = currentWinnerMessage[0];
+
   return (
     <>
       <Head>
@@ -38,6 +73,13 @@ const HomePage: NextPage<{ players: IPlayer[] }> = ({ players }) => {
       </Head>
 
       <Main>
+        <Round>{evaluations.length}º Rodada</Round>
+
+        <Message>
+          <em>&#34;{currentMessage.message}&#34;</em> -{" "}
+          {currentMessage.owner?.nickname}
+        </Message>
+
         <Scoreboard players={players} />
       </Main>
     </>
