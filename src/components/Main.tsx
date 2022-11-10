d40d66904894854af865083c3a7148b6621ef1d9
Miguel Rocha <miguelrocha.dev@gmail.com>
2022-11-10 22:07:01 +0000

    feat: create main component

---
+++ src/components/Main.tsx
@@ -0,0 +1,12 @@
+import { styled } from "../stitches/config";
+
+export const Main = styled("main", {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+
+  gap: "2rem",
+
+  padding: "2rem",
+});
